184693a803972ab559852646f484869e16892502
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-04 12:57:07 +0700

    Menambahkan deklarasi class mahasiswa (atribut & method)

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -3,3 +3,16 @@
 #include <string>
 using namespace std;
 
+class mahasiswa{
+    private:
+    int nim;
+    string nama;
+    public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa (string);
+    mahasiswa (int iNim, string iNama);
+    void cetak();
+
+};
+
