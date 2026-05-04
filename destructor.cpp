f5b8e8811157e460bc385711b793adb3510ab714
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-04 13:03:05 +0700

    Menambahkan destructor angka

--- destructor.cpp
+++ destructor.cpp
@@ -20,3 +20,10 @@ angka::angka(int i){ // Constructor
     isiData();
 }
 
+angka::~angka(){ // Destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
