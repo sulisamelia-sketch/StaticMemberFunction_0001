2b0f08b871303cdeb96113eef4e1dc05b89af949
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-04 13:04:15 +0700

    Menambahkan fungsi isiData

--- destructor.cpp
+++ destructor.cpp
@@ -35,3 +35,13 @@ void angka::cetakData()
     }
 }
 
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++) // loop input
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+
