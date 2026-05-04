2bef40bfbfff4069ecea183a71459e2eaa3019e2
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-05-04 13:03:38 +0700

    Menambahkan fungsi cetakData

--- destructor.cpp
+++ destructor.cpp
@@ -27,3 +27,11 @@ angka::~angka(){ // Destructor
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++) // loop cetak
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
